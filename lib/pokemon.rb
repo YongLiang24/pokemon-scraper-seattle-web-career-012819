10ca5f3a59bde69a7e4021edec1bea525fa6ce7d
github username <github email address>
2019-02-06 16:51:22 -0800

    second commit

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
   @@all = []
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db: nil)
     @id = id
     @name = name
     @type = type
@@ -21,7 +21,9 @@ class Pokemon
 
   def self.find(id, db)
     #db.execute("SELECT * FROM pokemon WHERE id = (?)", id)
-  
+    poke_row = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    row = poke_row.first
+    Pokemon.new(id: row[0], name: row[1], type: row[2])
   end
 
 end#end of class
